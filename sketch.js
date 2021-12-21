0f43f6637e2f47f444dfc57c35ea91513194d04f
shawnpeters1982 <96490631+shawnpeters1982@users.noreply.github.com>
2021-12-21 13:15:14 -0330

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let img;
+
+function preload() {
+  img = loadImage('https://static.boredpanda.com/blog/wp-content/uploads/2015/08/chemistry-elements-character-design-kcd-studios-6__605.jpg');
+}
+
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+
+function draw() {
+  background(220);
+  //image(img, 0, 0);
+  
+}
